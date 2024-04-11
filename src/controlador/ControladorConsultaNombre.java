3e26302bc42907b0a0ea266b21ca001435d4e217
FerrandoCarlos <cferrando2015@gmail.com>
2024-04-11 18:10:44 -0300

    Commit CF
    Se realizan cambios en vista búsqueda por nombre y en su controlador

--- src/controlador/ControladorConsultaNombre.java
+++ src/controlador/ControladorConsultaNombre.java
@@ -1,15 +1,50 @@
 package controlador;
 
-import vista.VistaConsultaNombre;
+import vista.*;
 
 /**
  *
  * @author nn
  */
 public class ControladorConsultaNombre {
+
     // public static  crear una variable de tipo vistaNombre
-     
-      public static void CargarInstancia(VistaConsultaNombre vn2) {
-           //cargar istancia de la vista usando la vaiable de tipo vista
+    public static VistaConsultaNombre vcn = new VistaConsultaNombre();
+
+    public static void CargarInstancia(VistaConsultaNombre vn2) {
+        //cargar istancia de la vista usando la vaiable de tipo vista
+        vcn = vn2;
+        promptTxt();
+
+    }
+    //Se crea placeholder
+    private static void promptTxt() {
+        ControladorMenuPrincipal.textPrompt(vcn.getjTFnombre(), "Ingrese el nombre del producto");
+    }
+    //Se busca producto
+    public static void buscarProducto() {
+        eliminarFilas();
+        for (modelo.Productos prod : ControladorMenuPrincipal.listaProductos) {
+
+            if (prod.getDescripcion().startsWith(vcn.getjTFnombre().getText())) {
+                ControladorMenuPrincipal.modeloTable.addRow(new Object[]{
+                        prod.getCodigo(),
+                        prod.getDescripcion(),
+                        prod.getPrecio(),
+                        prod.getStock() 
+                });
+            }
+
         }
+    }
+    //Se elimina filas de la busqueda
+    private static void eliminarFilas(){
+        
+        int f = vcn.getjTblDatos().getRowCount()-1;
+        for(;f>=0;f--){
+            ControladorMenuPrincipal.modeloTable.removeRow(f);
+        }
+    
+    }
+    //fin
 }
