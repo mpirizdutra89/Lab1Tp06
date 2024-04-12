70c9387d587137437f7955eb7302d957208b9fd0
aguz11 <agus110196@gmail.com>
2024-04-11 22:49:33 -0300

    Metodos y verificaciones en clase ControladorConsultaPrecio

--- src/controlador/ControladorCosultaPrecio.java
+++ src/controlador/ControladorCosultaPrecio.java
@@ -1,5 +1,7 @@
 package controlador;
 
+import java.awt.event.KeyEvent;
+import javax.swing.JOptionPane;
 import vista.VistaConsultaPrecio;
 
 /**
@@ -7,9 +9,68 @@ import vista.VistaConsultaPrecio;
  * @author nn
  */
 public class ControladorCosultaPrecio {
-     // public static  crear una variable de tipo vistaPrecio
+     public static VistaConsultaPrecio vcp = new VistaConsultaPrecio();
      
       public static void CargarInstancia(VistaConsultaPrecio cp) {
-           //cargar istancia de la vista usando la vaiable de tipo vista
+           vcp = cp;
+           validarEntradaNumerica(vcp.getTxtPrecioMin());
+           validarEntradaNumerica(vcp.getTxtPrecioMax());
+           promptTxt();
         }
+      
+       private static void promptTxt() {
+        ControladorMenuPrincipal.textPrompt(vcp.getTxtPrecioMin(), "Precio mínimo");
+        ControladorMenuPrincipal.textPrompt(vcp.getTxtPrecioMax(), "Precio máximo");
+    }
+       
+       private static void validarEntradaNumerica(javax.swing.JTextField textField) {
+        textField.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                char c = evt.getKeyChar();
+                String text = textField.getText();
+                
+                boolean puntoExistente = text.contains(".");
+                
+                if (!((c >= '0' && c <= '9') || (c == '.' && !puntoExistente) || c == KeyEvent.VK_BACK_SPACE || c == KeyEvent.VK_DELETE)) {
+                    evt.consume();
+                }
+            }
+        });
+    }
+       
+       public static void buscarProductosPorPrecio(){
+           
+        double precioMin = Double.parseDouble(vcp.getTxtPrecioMin().getText());
+        double precioMax = Double.parseDouble(vcp.getTxtPrecioMax().getText());
+        
+          if (precioMin <= 0 || precioMax < precioMin) {
+                if (precioMin <= 0) {
+                    ControladorMenuPrincipal.viewDialogo("El precio minimo no puedo ser menor que 0", "Error", JOptionPane.ERROR_MESSAGE);
+                } else {
+                    ControladorMenuPrincipal.viewDialogo("El precio maximimo debe ser mayor al minimo", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+                return;
+          }
+        
+        eliminarFilas();
+            
+             for (modelo.Productos prod : ControladorMenuPrincipal.listaProductos) {
+                if (prod.getPrecio() >= precioMin && prod.getPrecio() <= precioMax) {
+                    ControladorMenuPrincipal.modeloTable.addRow(new Object[]{
+                            prod.getCodigo(),
+                            prod.getDescripcion(),
+                            prod.getPrecio(),
+                            prod.getStock()
+                    });
+            }
+         }
+       }
+       
+        private static void eliminarFilas(){
+        
+        int f = vcp.getjTable1().getRowCount()-1;
+        for(;f>=0;f--){
+            ControladorMenuPrincipal.modeloTable.removeRow(f);
+        }
+    }
 }
--- src/vista/VistaConsultaPrecio.java
+++ src/vista/VistaConsultaPrecio.java
@@ -4,6 +4,9 @@
  */
 package vista;
 
+import javax.swing.JTable;
+import javax.swing.JTextField;
+
 /**
  *
  * @author nn
@@ -27,6 +30,7 @@ public class VistaConsultaPrecio extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+<<<<<<< Updated upstream
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
@@ -61,12 +65,55 @@ public class VistaConsultaPrecio extends javax.swing.JInternalFrame {
             }
         ));
         jScrollPane1.setViewportView(jTable1);
+=======
+        TxtPrecioMin = new javax.swing.JTextField();
+        TxtPrecioMax = new javax.swing.JTextField();
+        jDesktopPane1 = new javax.swing.JDesktopPane();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+
+        setTitle("Precio");
+>>>>>>> Stashed changes
+
+        TxtPrecioMin.setText("jTextField1");
+        TxtPrecioMin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TxtPrecioMinActionPerformed(evt);
+            }
+        });
+
+        TxtPrecioMax.setText("jTextField2");
+
+        javax.swing.GroupLayout jDesktopPane1Layout = new javax.swing.GroupLayout(jDesktopPane1);
+        jDesktopPane1.setLayout(jDesktopPane1Layout);
+        jDesktopPane1Layout.setHorizontalGroup(
+            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 513, Short.MAX_VALUE)
+        );
+        jDesktopPane1Layout.setVerticalGroup(
+            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+<<<<<<< Updated upstream
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(154, 154, 154)
@@ -84,11 +131,26 @@ public class VistaConsultaPrecio extends javax.swing.JInternalFrame {
                         .addContainerGap()
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 398, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+=======
+                .addComponent(jDesktopPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 453, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(TxtPrecioMin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(68, 68, 68)
+                .addComponent(TxtPrecioMax, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(157, 157, 157))
+>>>>>>> Stashed changes
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
+<<<<<<< Updated upstream
                 .addComponent(jLabel1)
                 .addGap(29, 29, 29)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -99,11 +161,22 @@ public class VistaConsultaPrecio extends javax.swing.JInternalFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 166, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(27, 27, 27))
+=======
+                .addComponent(jDesktopPane1)
+                .addGap(51, 51, 51)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(TxtPrecioMax, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(TxtPrecioMin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(41, 41, 41)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 231, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(71, 71, 71))
+>>>>>>> Stashed changes
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+<<<<<<< Updated upstream
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField1ActionPerformed
@@ -117,5 +190,30 @@ public class VistaConsultaPrecio extends javax.swing.JInternalFrame {
     private javax.swing.JTable jTable1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
+=======
+    public JTextField getTxtPrecioMax() {
+        return TxtPrecioMax;
+    }
+
+    public JTextField getTxtPrecioMin() {
+        return TxtPrecioMin;
+    }
+
+    public JTable getjTable1() {
+        return jTable1;
+    }
+
+    private void TxtPrecioMinActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TxtPrecioMinActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_TxtPrecioMinActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField TxtPrecioMax;
+    private javax.swing.JTextField TxtPrecioMin;
+    private javax.swing.JDesktopPane jDesktopPane1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+>>>>>>> Stashed changes
     // End of variables declaration//GEN-END:variables
 }
